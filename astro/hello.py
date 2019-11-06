11af327a29bb026a039f037a833d29dbdd9e760e
Ignacio Poggi <you@example.com>
2019-11-06 15:56:48 -0300

    HW

--- astro/hello.py
+++ astro/hello.py
@@ -1,10 +1,10 @@
-import datetime
+from datetime import datetime as dt
 
 fn = input('First name: ')
 ln = input('Last name: ')
 by = int(input('Birth year: '))
 
-current_year = datetime.datetime.now().year
+current_year = dt.now().year
 age = current_year - by
 
 print('Hello World, my name is {0} {1}, I am {2} years old, and I wrote this Python code!!'
